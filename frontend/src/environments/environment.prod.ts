90c54c4e9c5c0eb37e44a983bbbb18193ce99cc7
Alanna Risse <angrypirate@gmail.com>
2026-09-03 14:31:31 -0700

    removing mess

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://quote-keeper-production-854c.up.railway.app/api'
+  apiUrl: 'https://quote-keeper-production-cf91.up.railway.app/api'
 };
